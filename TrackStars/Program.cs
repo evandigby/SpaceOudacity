796d9e73c5fc1b56be5f9d4d902f4f0e6e206ce2
Evan Digby <evandigby@gmail.com>
2023-10-08 22:31:49 -0700

    working

--- TrackStars/Program.cs
+++ TrackStars/Program.cs
@@ -1,10 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
 using OpenCvSharp;
 using OpenCvSharp.Features2D;
 
-const int maxFrames = 3 * 30;
+const int maxFrames = 100 * 30;
+const int maxHue = 359;
+double minHueWavelength = HueToWavelength(0);
+double minMaxHueWaveLengthDelta = HueToWavelength(maxHue) - HueToWavelength(0);
 
-using var orb = ORB.Create(500); // 5000, 1.2f, 8, 31, 0, 4, ORBScoreType.Harris, 31, 10);
+const int sampleRate = 44100;
+const int signalChannels = 2;
+var oneFrame = TimeSpan.FromSeconds((double)1 / 30);
+
+var signalWaveFormat = WaveFormat.CreateIeeeFloatWaveFormat(sampleRate, signalChannels);
+var outputWaveFormat = WaveFormat.CreateIeeeFloatWaveFormat(sampleRate, 2);
+
+using var orb = ORB.Create(500);
 
 var p = new SimpleBlobDetector.Params
 {
@@ -28,25 +40,20 @@ using (var capture = VideoCapture.FromFile(@"E:\SpaceApps\NebulaVideo.mov"))
 {
     var descriptorsLast = new Mat();
     KeyPoint[]? keypointsLast = null;
-    Mat? lastThresh = null;
 
     using var output = new VideoWriter(@"E:\SpaceApps\NebulaVideoOutputDetection.avi", FourCC.WMV1, capture.Fps, new Size(capture.FrameWidth, capture.FrameHeight), true);
 
     for (int i = 0; i < maxFrames; i++)
     {
-        //using var greyscaleImageMat = new Mat();
         using var blur = new Mat();
         using var outImg = new Mat();
-
-        var thresh = new Mat();
+        using var thresh = new Mat();
 
         var currentFrame = capture.PosFrames;
         using var img = capture.RetrieveMat();
         if (img.Empty()) break;
 
         Cv2.ExtractChannel(img, thresh, 0);
-        //Cv2.GaussianBlur(greyscaleImageMat, blur, new Size(5, 5), 0);
-        //Cv2.Threshold(blur, thresh, 200, 255, ThresholdTypes.Binary);
 
         var keypoints = sbd.Detect(thresh);
 
@@ -58,13 +65,6 @@ using (var capture = VideoCapture.FromFile(@"E:\SpaceApps\NebulaVideo.mov"))
         if (i > 0 && keypointsLast != null)
         {
             var matches = bf.KnnMatch(descriptorsLast, descriptors, 1).Select(m => m[0]).OrderByDescending(m => m.Distance).ToArray();
-
-            //if (lastThresh != null)
-            //{
-            //    using var outMatchImage = new Mat();
-            //    Cv2.DrawMatches(lastThresh, keypointsLast, thresh, keypoints, matches, outMatchImage, Scalar.Red, flags: DrawMatchesFlags.DrawRichKeypoints);
-            //    Cv2.ImWrite($@"E:\SpaceApps\Test\{i}.jpg", outMatchImage);
-            //}
 
             var frame1 = i - 1;
             var frame2 = i;
@@ -92,19 +92,10 @@ using (var capture = VideoCapture.FromFile(@"E:\SpaceApps\NebulaVideo.mov"))
                     matchObjects.Add(matchObject);
                 }
 
-                DMatch? bestMatch = matchObject.Matches[frame2];
-
-                if (bestMatch is not null)
-                {
-                    bestMatch = match.Distance > bestMatch.Value.Distance ? bestMatch : match;
-                }
-
-                bestMatch ??= match;
-
-                matchObject.Matches[frame2] = bestMatch;
-                matchObject.Index[frame2] = bestMatch.Value.TrainIdx;
-                matchObject.KeyPoints[frame2] = keypoints[bestMatch.Value.TrainIdx];
-                newSecondToFirst[bestMatch.Value.TrainIdx] = matchObjectIndex;
+                matchObject.Matches[frame2] = match;
+                matchObject.Index[frame2] = match.TrainIdx;
+                matchObject.KeyPoints[frame2] = keypoints[match.TrainIdx];
+                newSecondToFirst[match.TrainIdx] = matchObjectIndex;
             }
 
             output.Write(outImg);
@@ -112,15 +103,13 @@ using (var capture = VideoCapture.FromFile(@"E:\SpaceApps\NebulaVideo.mov"))
 
         secondToFirst = newSecondToFirst;
         descriptorsLast?.Dispose();
-        lastThresh?.Dispose();
-        lastThresh = thresh;
         descriptorsLast = descriptors;
         keypointsLast = keypoints;
         Console.WriteLine($"Detected frame {i}");
     }
 }
 
-var matchObjectWithAllFrames = matchObjects.OrderByDescending(mo => mo.KeyPoints.Count(kp => kp is not null)).Take(20).ToArray();
+var bestMatchObjects = matchObjects.OrderByDescending(mo => mo.KeyPoints.Count(kp => kp is not null)).Take(20).ToArray();
 
 var test = matchObjects
     .OrderByDescending(mo => mo.KeyPoints.Count(kp => kp is not null))
@@ -162,10 +151,11 @@ foreach (var obj in test)
 }
 
 
+var totalFrames = 0;
 using (var capture = VideoCapture.FromFile(@"E:\SpaceApps\NebulaVideo.mov"))
 {
     using var output = new VideoWriter(@"E:\SpaceApps\NebulaVideoOutputMatched.avi", FourCC.WMV1, capture.Fps, new Size(capture.FrameWidth, capture.FrameHeight), true);
-    for (int i = 0; i < maxFrames; i++)
+    for (int i = 0; i < maxFrames; i++, totalFrames++)
     {
         var currentFrame = capture.PosFrames;
         using var img = capture.RetrieveMat();
@@ -173,11 +163,22 @@ using (var capture = VideoCapture.FromFile(@"E:\SpaceApps\NebulaVideo.mov"))
 
         using var outImg = new Mat();
 
-        var matchedWithNotNull = matchObjectWithAllFrames.Where(o => o.KeyPoints[i] is not null).ToArray();
+        var matchedWithNotNull = bestMatchObjects.Where(o => o.KeyPoints[i] is not null).ToArray();
 
-        var matchedKeyPoints = matchedWithNotNull.Select(o => o.KeyPoints[i]).Cast<KeyPoint>().ToArray();
+        var matchedKeyPoints = new List<KeyPoint>();
 
-        if (matchedKeyPoints.Length > 0)
+        foreach (var obj in bestMatchObjects)
+        {
+            var kp = obj.KeyPoints[i];
+            if (kp != null)
+            {
+                var bgr = img.At<Vec3b>((int)kp.Value.Pt.Y, (int)kp.Value.Pt.X);
+                obj.Vecs[i] = bgr;
+                matchedKeyPoints.Add(kp.Value);
+            }
+        }
+
+        if (matchedKeyPoints.Count > 0)
         {
             Cv2.DrawKeypoints(img, matchedKeyPoints, outImg, Scalar.Purple, DrawMatchesFlags.DrawRichKeypoints);
             output.Write(outImg);
@@ -190,46 +191,107 @@ using (var capture = VideoCapture.FromFile(@"E:\SpaceApps\NebulaVideo.mov"))
     }
 }
 
-//Console.WriteLine(matchesBag.Count);
+foreach (var obj in bestMatchObjects)
+{
+    for (int i = 0; i < totalFrames;)
+    {
+        var bgr = obj.Vecs[i];
+        if (bgr == null)
+        {
+            var streak = obj.Vecs.Skip(i).TakeWhile(v => v is null).ToList();
 
-//// From: https://learn.microsoft.com/en-us/dotnet/standard/parallel-programming/walkthrough-creating-a-custom-dataflow-block-type
-//IPropagatorBlock<T, T[]> CreateSlidingWindow<T>(int windowSize)
-//{
-//    // Create a queue to hold messages.
-//    var queue = new Queue<T>();
+            obj.SampleProviders.Add(new SilenceProvider(signalWaveFormat).ToSampleProvider().Take(oneFrame * streak.Count));
+            i += streak.Count;
+        }
+        else
+        {
+            var streak = obj.Vecs.Skip(i).TakeWhile(v => v is not null).ToList();
 
-//    // The source part of the propagator holds arrays of size windowSize
-//    // and propagates data out to any connected targets.
-//    var source = new BufferBlock<T[]>();
+            var sig = ColorToSignal(bgr.Value, 0.2);
 
-//    // The target part receives data and adds them to the queue.
-//    var target = new ActionBlock<T>(item =>
-//    {
-//        // Add the item to the queue.
-//        queue.Enqueue(item);
-//        // Remove the oldest item when the queue size exceeds the window size.
-//        if (queue.Count > windowSize)
-//            queue.Dequeue();
-//        // Post the data in the queue to the source block when the queue size
-//        // equals the window size.
-//        if (queue.Count == windowSize)
-//            source.Post(queue.ToArray());
-//    });
+            obj.SampleProviders.Add(sig.Take(oneFrame * streak.Count));
+            i += streak.Count;
+        }
+    }
+}
 
-//    // When the target is set to the completed state, propagate out any
-//    // remaining data and set the source to the completed state.
-//    target.Completion.ContinueWith(delegate
-//    {
-//        if (queue.Count > 0 && queue.Count < windowSize)
-//            source.Post(queue.ToArray());
-//        source.Complete();
-//    });
+var mixer = new MixingSampleProvider(outputWaveFormat);
 
-//    // Return a IPropagatorBlock<T, T[]> object that encapsulates the
-//    // target and source blocks.
-//    return DataflowBlock.Encapsulate(target, source);
-//}
+foreach (var obj in bestMatchObjects)
+{
+    var samples = new ConcatenatingSampleProvider(obj.SampleProviders);
+    mixer.AddMixerInput(samples);
+}
 
+var totalFramesTime = oneFrame * totalFrames;
+
+WaveFileWriter.CreateWaveFile(@"E:\SpaceApps\test.wav", mixer.Take(totalFramesTime).ToWaveProvider());
+
+ISampleProvider ColorToSignal(Vec3b color, double gain)
+{
+    var (h, _, _) = RGBToHSV(color);
+    var wave = HueToWavelength(h); ;
+    return LightWaveToSignal(wave, gain);
+}
+
+ISampleProvider LightWaveToSignal(double wavelength, double gain)
+{
+    var minimized = wavelength - minHueWavelength;
+
+    var waveRatio = minimized / minMaxHueWaveLengthDelta;
+
+    const double pianoMinFrequency = 27.5d;
+    const double pianoMaxFrequency = 1000d;//186.009d;
+    const double pianoDelta = pianoMaxFrequency - pianoMinFrequency;
+
+    var frequency = (waveRatio * pianoDelta) - pianoMinFrequency;
+
+    return new SignalGenerator(sampleRate, signalChannels)
+    {
+        Gain = gain,
+        Frequency = frequency,
+        Type = SignalGeneratorType.Sin,
+    };
+}
+
+static double HueToWavelength(double hue) => 650d - (250d / maxHue * hue);
+
+static (double, double, double) RGBToHSV(Vec3b rgb)
+{
+    var (b, g, r) = rgb;
+
+    double delta, min;
+    double h = 0, s, v;
+
+    min = Math.Min(Math.Min(r, g), b);
+    v = Math.Max(Math.Max(r, g), b);
+    delta = v - min;
+
+    if (v == 0.0)
+        s = 0;
+    else
+        s = delta / v;
+
+    if (s == 0)
+        h = 0.0;
+
+    else
+    {
+        if (r == v)
+            h = (g - b) / delta;
+        else if (g == v)
+            h = 2 + ((b - r) / delta);
+        else if (b == v)
+            h = 4 + ((r - g) / delta);
+
+        h *= 60;
+
+        if (h < 0.0)
+            h = h + maxHue;
+    }
+
+    return (h, s, v / 255);
+}
 
 internal record MatFrame(int Frame, Mat Mat);
 
@@ -248,181 +310,13 @@ internal class MatchObject
         Index = Enumerable.Range(0, numFrames).Select(_ => -1).ToArray();
         KeyPoints = new KeyPoint?[numFrames];
         Matches = new DMatch?[numFrames];
+        Vecs = new Vec3b?[numFrames];
     }
 
     public int[] Index { get; }
     public KeyPoint?[] KeyPoints { get; }
     public DMatch?[] Matches { get; }
+    public List<ISampleProvider> SampleProviders { get; } = new();
+    public Vec3b?[] Vecs { get; }
 }
 
-
-
-//var inputFolder = @"E:\SpaceApps\Nebula";
-
-//var outFolder = Path.Combine(inputFolder, "out");
-
-//Directory.CreateDirectory(outFolder);
-
-//int totalFrames = 0;
-
-//var matDetectBlobs = new TransformBlock<MatFrame, MatFrameBlobs>((matFile) =>
-//{
-//    var rows = matFile.Mat.Rows;
-//    var cols = matFile.Mat.Cols;
-
-//    var typ = matFile.Mat.Type();
-
-//    using var greyscaleImageMat = new Mat();
-//    using var blur = new Mat();
-//    var thresh = new Mat();
-//    var descriptors = new Mat();
-
-//    Cv2.ExtractChannel(matFile.Mat, greyscaleImageMat, 0);
-//    Cv2.GaussianBlur(greyscaleImageMat, blur, new Size(5, 5), 0);
-//    Cv2.Threshold(blur, thresh, 200, 255, ThresholdTypes.Binary);
-
-//    var keypoints = sbd.Detect(thresh);
-//    orb.Compute(thresh, ref keypoints, descriptors);
-//    //orb.DetectAndCompute(thresh, null, out var keypoints, descriptors);
-
-//    return new MatFrameBlobs(matFile, thresh, descriptors, keypoints.ToList());
-//},
-//new ExecutionDataflowBlockOptions
-//{
-//    MaxDegreeOfParallelism = 16,
-//});
-
-//var matDrawBlobs = new TransformBlock<MatFrameBlobs, MatFrameBlobsImage>((matFile) =>
-//{
-//    var input = InputArray.Create(matFile.Greyscale);
-//    var output = InputOutputArray.Create(matFile.Greyscale) ?? throw new Exception("null output");
-
-//    Cv2.DrawKeypoints(input, matFile.KeyPoints, output, color: Scalar.Purple, flags: DrawMatchesFlags.DrawRichKeypoints);
-//    return new MatFrameBlobsImage(matFile, output.GetMat()!);
-//},
-//new ExecutionDataflowBlockOptions
-//{
-//    MaxDegreeOfParallelism = 16,
-//});
-
-//var matSlidingWindow = CreateSlidingWindow<MatFrameBlobs>(2);
-
-//var detectMatches = new TransformBlock<MatFrameBlobs[], MatFrameBlobsMatches>(mats =>
-//{
-//    if (mats.Length != 2)
-//    {
-//        throw new Exception("bad mats length");
-//    }
-
-//    var f1 = mats[0];
-//    var f2 = mats[1];
-
-//    var des1 = f1.Descriptors;
-//    var des2 = f2.Descriptors;
-
-//    var matches = bf.Match(des1, des2);
-
-//    var sorted = matches
-//        .OrderBy(m => m.Distance)
-//        //.Take(50)
-//        .ToArray();
-
-//    return new MatFrameBlobsMatches(f1, f2, sorted);
-//},
-//new ExecutionDataflowBlockOptions
-//{
-//    MaxDegreeOfParallelism = 16,
-//});
-
-
-//var matchesBag = new ConcurrentBag<MatchKeyPoints>();
-
-//var matchesOutput = new TransformBlock<MatFrameBlobsMatches, MatFrameBlobs>(mats =>
-//{
-//    matchesBag.Add(new MatchKeyPoints(mats.F1.Mat.Frame, mats.F2.Mat.Frame, mats.F1.KeyPoints, mats.F2.KeyPoints, mats.Matches));
-
-//    return mats.F1;
-//},
-//new ExecutionDataflowBlockOptions
-//{
-//    MaxDegreeOfParallelism = 16,
-//});
-
-//var cleanup = new ActionBlock<MatFrameBlobs>(matFile =>
-//{
-//    var currentFinished = Interlocked.Increment(ref totalFrames);
-
-//    Console.WriteLine($"Finished {currentFinished} frames");
-
-//    matFile.Mat.Mat.Dispose();
-//    matFile.Greyscale.Dispose();
-//    matFile.Descriptors.Dispose();
-//});
-
-
-//var dflo = new DataflowLinkOptions
-//{
-//    PropagateCompletion = true,
-//};
-
-//matDetectBlobs.LinkTo(matSlidingWindow, dflo);
-//matSlidingWindow.LinkTo(detectMatches, dflo);
-//detectMatches.LinkTo(matchesOutput, dflo);
-//matchesOutput.LinkTo(cleanup, dflo);
-
-//using (var capture = VideoCapture.FromFile(@"E:\SpaceApps\NebulaVideo.mov"))
-//{
-//    for (int i = 0; i < maxFrames; i++)
-//    //while (true)
-//    {
-//        var currentFrame = capture.PosFrames;
-//        var img = capture.RetrieveMat();
-
-//        if (img.Empty()) break;
-
-//        await matDetectBlobs.SendAsync(new MatFrame(currentFrame, img));
-//    }
-
-//    matDetectBlobs.Complete();
-
-//    await cleanup.Completion;
-//}
-
-//var matchesList = matchesBag.OrderBy(mkp => mkp.FrameOne).ToList();
-
-//var matchObjects = new List<MatchObject>();
-
-//var secondToFirst = new Dictionary<int, int>();
-
-//for (int i = 0; i < matchesList.Count; i++)
-//{
-//    var matches = matchesList[i];
-
-//    var newSecondToFirst = new Dictionary<int, int>();
-//    foreach (var match in matches.Matches)
-//    {
-//        MatchObject matchObject;
-//        if (secondToFirst.TryGetValue(match.QueryIdx, out var matchObjectIndex))
-//        {
-//            matchObject = matchObjects[matchObjectIndex];
-
-//            if (matchObject.Index[matches.FrameOne] != match.QueryIdx)
-//            {
-//                throw new Exception("Algorithm wrong");
-//            }
-//        }
-//        else
-//        {
-//            matchObject = new MatchObject(totalFrames + 1);
-//            matchObject.Index[matches.FrameOne] = match.QueryIdx;
-//            matchObject.KeyPoints[matches.FrameOne] = matches.F1[match.QueryIdx];
-
-//            matchObjectIndex = matchObjects.Count;
-//            matchObjects.Add(matchObject);
-//        }
-//        matchObject.Index[matches.FrameTwo] = match.TrainIdx;
-//        matchObject.KeyPoints[matches.FrameTwo] = matches.F2[match.TrainIdx];
-//        newSecondToFirst[match.TrainIdx] = matchObjectIndex;
-//    }
-//    secondToFirst = newSecondToFirst;
-//}
